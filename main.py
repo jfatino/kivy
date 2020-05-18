e14903609e019486b7b41e3b44d5d457209a0ee3
Jason <jfatino@gmail.com>
2020-05-18 00:08:59 -0400

    Text in box also start of separate class file

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-import kivy
+# I don't know what I'm doing and it's fun :)
+import os
+from SqlTools import Test
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.config import Config
 
 
-class MyGrid():
+class MyGrid(GridLayout):
     def __init__(self,**kwargs):
         super(MyGrid, self).__init__(**kwargs)
-        self.cols =2
+        self.cols = 2
         self.add_widget(Label(text="Name: "))
         self.name = TextInput(multiline = False)
-        self.adw
+        self.add_widget(self.name)
+        # quick test to see if I can pre-fill the tb
+        
+        self.name.text="Hello"
 
 class MyApp(App):
     title = "Testing"
-                
+    Config.set('graphics', 'resizable', False)            
     def build(self):
-        return Label(text="Hello")
+        return MyGrid()
                    
     
 
